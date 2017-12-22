a09171954343b4e70cd1fb17b4e02d1624e160eb
stmoree@gmail.com <stmoree@gmail.com>
2017-12-22 04:00:39 -0800

    added database and web api project

---
+++ WebSite2/Scripts/details_paget.js
@@ -0,0 +1,13 @@
+﻿angular.module('ClanMembers', [])
+    .controller('Controller', ['$scope', function ($scope) {
+        $scope.customer = {
+            name: 'Naomi',
+            address: '1600 Amphitheatre'
+        };
+    }])
+    .directive('detailsPage', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '../OtakuLordss.html'
+        };
+    });
